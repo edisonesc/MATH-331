2437bdd2bc8fad225e60f0011e1386ea8c45ec77
edisonesc <edi.escx@gmail.com>
2018-12-12 22:08:56 +0800

    added machine problem 3

---
+++ MachineProblem-Math331-III/src/machineproblem/math331/iii/MachineProblemMath331III.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package machineproblem.math331.iii;
+
+/**
+ *
+ * @author IT-301
+ */
+public class MachineProblemMath331III {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
